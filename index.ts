e62bccb9d0a2aa5869df82fca8656182b7e6cebb
alongw <alongw@alongw.cn>
2023-07-14 22:44:17 +0800

    feat: improving the pay-code plugin

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // 引入 Router 实例
-import { Router } from 'express'
+import { Router, static as ExpressStatic } from 'express'
+import path from 'path'
 
 // 实例化 Router 对象
 const router = Router()
@@ -8,6 +9,8 @@ const router = Router()
 router.use('/', async (req, res, next) =>
     (await import('./router/getPayCode')).default(req, res, next)
 )
+
+router.use('/static', ExpressStatic(path.join(__dirname, 'public')))
 
 // 导出路由插件简介名称说明 （ 仅用于显示方便辨认 ）
 export const Name = '收款码'
